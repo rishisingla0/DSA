9906adeb792f3131f712ad651a77424a44ffcff1
Rishi Singla <rishisingla36@gmail.com>
2026-09-12 01:28:52 +0530

    Time: 4 ms (36.28%) | Memory: 43.6 MB (9.82%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -4,15 +4,16 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (!stack.isEmpty()) {
-                if (ch == ')' && stack.peek() == '(' || ch == ']' && stack.peek() == '['
-                        || ch == '}' && stack.peek() == '{') {
-                    stack.pop();
-                } else {
-                    stack.push(ch);
-                }
-            } else {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
+            } else if (stack.isEmpty()) {
+                return false;
+            } else if (ch == ')' && stack.peek() == '(' || ch == '}' && stack.peek() == '{'
+                    || ch == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
             }
         }
         return stack.isEmpty();
