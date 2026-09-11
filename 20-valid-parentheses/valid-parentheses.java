981a6a741da39242800a027ba90c52fa4f462634
Rishi Singla <rishisingla36@gmail.com>
2026-09-12 01:15:30 +0530

    Time: 4 ms (36.28%) | Memory: 43.1 MB (72.54%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!stack.isEmpty()) {
+                if (ch == ')' && stack.peek() == '(' || ch == ']' && stack.peek() == '['
+                        || ch == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
