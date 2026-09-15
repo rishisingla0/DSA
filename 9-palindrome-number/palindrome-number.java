29b362812f0c6d7ae27927f31bc319d2df7b0288
Rishi Singla <rishisingla36@gmail.com>
2026-09-15 21:11:48 +0530

    Time: 6 ms (22.40%) | Memory: 45.8 MB (71.77%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int ori = x;
+        int rev = 0;
+        while( x > 0) {
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+        return ori == rev;
+    }
+}
